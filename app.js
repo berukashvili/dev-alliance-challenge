481a883af08126013d7af47b1443941b66dfa316
Luka Berukashvili <lukaberukashvili@gmail.com>
2022-11-14 15:20:17 +0400

    features: refactor code

--- app.js
+++ app.js
@@ -1,118 +1,132 @@
 "use strict";
 
 // DOM Elements
-const table = document.querySelector(".table");
 const form = document.querySelector(".form");
-const tableBody = document.querySelector(".list-body");
-const rowBody = document.querySelector(".thread");
+const table = document.querySelector(".table");
+const submitBtn = document.querySelector(".submit-btn");
+const tableList = document.querySelector(".list-body");
 const remove = document.querySelector(".remove");
 
 let dataArr = [];
 let count = 0;
 
-// Add Form Data to Local Storage
-const addData = (event) => {
-  event.preventDefault();
-  count++;
-
-  getData();
-
-  dataArr.push({
-    count: count,
-    firstName: document.getElementById("validationCustom01").value,
-    lastName: document.getElementById("validationCustom02").value,
-    address: document.getElementById("validationCustomUsername").value,
-    date: document.getElementById("validationCustom03").value,
-    gender: document.getElementById("validationCustom04").value,
-    note: document.getElementById("validationCustom05").value,
-  });
-
-  for (let i = 0; i < dataArr.length; i++) {
-    localStorage.setItem(`${i}`, JSON.stringify(dataArr[i]));
-  }
-  showData();
-};
-
-// Parse And Add Local data Into Array
-const getData = () => {};
-
-// Maintain and Show Entered Data
-const showData = () => {
-  getData();
-
-  let num = table.rows.length;
-  while (--num) {
-    table.deleteRow(num);
-  }
-
-  for (let i = 0; i < localStorage.length; i++) {
-    const localItem = JSON.parse(localStorage.getItem(`${i}`));
-    if (localItem != null) {
-      const row = table.insertRow(1);
-
-      const countCell = row.insertCell(0);
-      countCell.innerHTML = localItem.count;
-
-      const firstNameCell = row.insertCell(1);
-      firstNameCell.innerHTML = localItem.firstName;
-
-      const lastNameCell = row.insertCell(2);
-      lastNameCell.innerHTML = localItem.lastName;
-
-      const addressCell = row.insertCell(3);
-      addressCell.innerHTML = localItem.address;
-
-      const dateCell = row.insertCell(4);
-      dateCell.innerHTML = localItem.date;
-
-      const genderCell = row.insertCell(5);
-      genderCell.innerHTML = localItem.gender;
-
-      const noteCell = row.insertCell(6);
-      noteCell.innerHTML = localItem.note;
-
-      //Create and Append Remove Button
-      const removeBtn = document.createElement("button");
-      removeBtn.innerHTML = "X";
-      removeBtn.classList.add("btn", "btn-primary");
-      remove.appendChild(removeBtn);
-
-      //Remove Item
-      const removeRow = () => {
-        localStorage.removeItem(`${i}`);
-        showData();
-      };
-
-      const removeCell = row.insertCell(7);
-      removeCell.appendChild(removeBtn);
-
-      removeBtn.addEventListener("click", removeRow);
+const localInit = function () {
+  if (localStorage.localData) {
+    dataArr = JSON.parse(localStorage.localData);
+    for (let i = 0; i < dataArr.length; i++) {
+      makeRow(
+        i,
+        dataArr[i].count,
+        dataArr[i].firstName,
+        dataArr[i].lastName,
+        dataArr[i].address,
+        dataArr[i].date,
+        dataArr[i].gender,
+        dataArr[i].note
+      );
     }
   }
 };
 
-window.addEventListener("load", showData);
+const handleInput = function (event) {
+  event.preventDefault();
 
-form.addEventListener("submit", addData);
+  const rowCount = count;
+  const firstName = document.getElementById("validationCustom01").value;
+  const lastName = document.getElementById("validationCustom02").value;
+  const address = document.getElementById("validationCustomUsername").value;
+  const date = document.getElementById("validationCustom03").value;
+  const gender = document.getElementById("validationCustom04").value;
+  const note = document.getElementById("validationCustom05").value;
 
-// Validation
-(() => {
-  const forms = document.querySelectorAll(".needs-validation");
+  const dataObj = {
+    count: count,
+    firstName: firstName,
+    lastName: lastName,
+    address: address,
+    date: date,
+    gender: gender,
+    note: note,
+  };
 
-  Array.from(forms).forEach((form) => {
-    form.addEventListener(
-      "submit",
-      (event) => {
-        if (!form.checkValidity()) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
+  dataArr.push(dataObj);
 
-        form.classList.add("was-validated");
-      },
-      false
-    );
-  });
-})();
+  localStorage.localData = JSON.stringify(dataArr);
+
+  makeRow(i, count, firstName, lastName, address, date, gender, note);
+
+  // document.getElementById("validationCustom01").value = "";
+  // document.getElementById("validationCustom02").value = "";
+  // document.getElementById("validationCustomUsername").value = "";
+  // document.getElementById("validationCustom03").value = "";
+  // document.getElementById("validationCustom04").value = "";
+  // document.getElementById("validationCustom05").value = "";
+};
+
+const makeRow = (
+  index,
+  count,
+  firstName,
+  lastName,
+  address,
+  date,
+  gender,
+  note
+) => {
+  const row = table.insertRow();
+
+  const countCell = row.insertCell(0);
+  countCell.innerHTML = count;
+
+  const firstNameCell = row.insertCell(1);
+  firstNameCell.innerHTML = firstName;
+
+  const lastNameCell = row.insertCell(2);
+  lastNameCell.innerHTML = lastName;
+
+  const addressCell = row.insertCell(3);
+  addressCell.innerHTML = address;
+
+  const dateCell = row.insertCell(4);
+  dateCell.innerHTML = date;
+
+  const genderCell = row.insertCell(5);
+  genderCell.innerHTML = gender;
+
+  const noteCell = row.insertCell(6);
+  noteCell.innerHTML = note;
+
+  const removeCell = row.insertCell(7);
+  removeCell.innerHTML =
+    '<button class="btn btn-primary remove-btn">Remove</Button>';
+};
+
+const removeRow = function (index) {
+  table.deleteRow(index);
+};
+
+// (
+//   // Validation
+//   () => {
+//     const forms = document.querySelectorAll(".needs-validation");
+
+//     Array.from(forms).forEach((form) => {
+//       form.addEventListener(
+//         "submit",
+//         (event) => {
+//           if (!form.checkValidity()) {
+//             event.preventDefault();
+//             event.stopPropagation();
+//           }
+
+//           form.classList.add("was-validated");
+//         },
+//         false
+//       );
+//     });
+//   }
+// )();
+
+form.addEventListener("submit", handleInput);
 
 const submitData = [];
